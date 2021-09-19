a8b1e7b3a321f62b41f05fb6e5373edb42f761da
VBelles <vicentbelles@gmail.com>
2021-09-19 19:26:44 +0200

    Initial commit

---
+++ desktop/src/com/momentum/game/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.momentum.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.momentum.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Galaxy Momentum";
+		new LwjglApplication(new Game(), config);
+	}
+}
