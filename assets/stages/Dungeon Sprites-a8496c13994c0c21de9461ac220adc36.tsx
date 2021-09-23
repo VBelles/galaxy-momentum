a67c153d4d25d2bcb72fe1a4056aac7e1ea24095
VBelles <vicentbelles@gmail.com>
2021-09-23 16:11:56 +0000

    Deploying to github-pages from  @ eed6f376c8b18f1504ff681fcc89f4fa4d280320 🚀

---
+++ assets/stages/Dungeon Sprites-a8496c13994c0c21de9461ac220adc36.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dungeon Sprites" tilewidth="16" tileheight="16" tilecount="190" columns="19">
+ <image source="Dungeon Sprites.png" width="304" height="160"/>
+ <tile id="131">
+  <properties>
+   <property name="killer" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
