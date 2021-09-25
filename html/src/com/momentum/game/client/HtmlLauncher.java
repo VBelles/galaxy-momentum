72effeae8c6ea44512cca8f201ccca4659e95abc
VBelles <vicentbelles@gmail.com>
2021-09-25 13:19:02 +0200

    Modified web configuration

--- html/src/com/momentum/game/client/HtmlLauncher.java
+++ html/src/com/momentum/game/client/HtmlLauncher.java
@@ -3,6 +3,7 @@ package com.momentum.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.GwtGraphics;
 import com.momentum.game.Game;
 
 public class HtmlLauncher extends GwtApplication {
@@ -10,7 +11,11 @@ public class HtmlLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
                 // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
+                GwtApplicationConfiguration configuration = new GwtApplicationConfiguration(true);
+                configuration.padHorizontal = 0;
+                configuration.padVertical = 0;
+                configuration.fullscreenOrientation = GwtGraphics.OrientationLockType.LANDSCAPE;
+                return configuration;
                 // Fixed size application:
                 //return new GwtApplicationConfiguration(1280, 720);
         }
